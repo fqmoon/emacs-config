ea9a9da36b20b414a1bd3aa1a8494d234ab7ede8
njw <mail@fqmoon.com>
2025-01-12 23:19:37 +0800

    文件自动保存、文件同步修改

--- my-config/my-other.el
+++ my-config/my-other.el
@@ -7,6 +7,12 @@
 
 ;; 禁止备份文件（后缀名为~）
 (setq make-backup-files nil)
+;; 禁止自动保存
+(setq auto-save-default nil)
+
+;; 自动同步文件修改
+;; 加载修改后文件在Emcas中被称为revert
+(global-auto-revert-mode 1)
 
 ;; 鼠标滚轮
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
