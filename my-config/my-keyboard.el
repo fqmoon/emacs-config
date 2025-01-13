74c28c6df4bf0c16b140c8ac9effa047a10460d2
倪俊文 <nijunwen@nijunwendeMacBook-Air.local>
2025-01-13 18:27:16 +0800

    macOS改键位

--- my-config/my-keyboard.el
+++ my-config/my-keyboard.el
@@ -1,7 +1,8 @@
 ;; 默认情况下在macOS上，option键是super，而cmd是meta
 ;; 虽然键位变成WIN键位了，但不符合macOS的传统，交换过来
-(setq mac-option-modifier 'meta
-      mac-command-modifier 'super)
+;;(setq mac-option-modifier 'meta
+;;      mac-command-modifier 'super)
+;; 没有启用，因为键位位置更重要，而非功能
 
 ;; MacBook没有右ctrl键，只有右option键
 ;; Emacs大量的命令都是要用ctrl键的，将右option改成右ctrl
