825bdf3129157eeeca6c5ad74faef99202fd7007
倪俊文 <nijunwen@nijunwendeMacBook-Air.local>
2025-01-16 23:24:35 +0800

    用ues-pcakge配置org-mode

--- my-config/my-org.el
+++ my-config/my-org.el
@@ -1,19 +1,25 @@
-;;; org-mode config
+1;;; org-mode config
 
 ;; 在中英文之间插入空格的包
 (use-package pangu-spacing)
 
-(add-hook 'org-mode-hook
-	  (lambda ()
-	    ;; 不截断，也就是折行的意思
-	    (setq truncate-lines nil)
-	    ;; point在行内时起headline
-	    (setq org-M-RET-may-split-line nil)
-	    ;; 在中英文之间加入空格
-	    (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
-	    (pangu-spacing-mode t)))
+(defun fq-org-config ()
+  "Config org-mode."
+  ;; 不截断，也就是折行的意思
+  (setq truncate-lines nil)
+  ;; 在中英文之间加入空格
+  (set (make-local-variable 'pangu-spacing-real-insert-separtor) t)
+  (pangu-spacing-mode t))
 
-;; bind
-(define-key org-mode-map (kbd "C-j") 'avy-goto-char-timer)
+(use-package org
+  :ensure nil
+  :defer t
+  :hook ((org-mode . fq-org-config))
+  :bind (
+	 :map org-mode-map
+	 ("C-j" . avy-goto-char-timer))
+  :config
+  ;; point在行内时起headline
+  (setq org-M-RET-may-split-line nil))
 
 (provide 'my-org)
