2982b3b3b88190a6168224825c7d4e514ca23bb8
倪俊文 <nijunwen@nijunwendeMacBook-Air.local>
2025-01-18 22:10:23 +0800

    项目级字符串搜索，类似于VSCODE的全局搜索

--- my-config/my-search.el
+++ my-config/my-search.el
@@ -1,5 +1,8 @@
 ;; search tool
 (use-package consult
+  :bind (
+	 ;; 项目级字符串搜索
+	 ("C-S-s" . 'consult-ripgrep))
   :config
   ;; 在安装了consult之后，consult-line命令就可以不只匹配一行的开头了
   (use-package embark-consult
