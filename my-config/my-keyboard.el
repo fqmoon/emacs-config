ebd49dd9ddb5d1fda942e1855f925ba96e5f57b2
倪俊文 <nijunwen@nijunwendeMacBook-Air.local>
2025-01-18 22:05:03 +0800

    macOS修饰键映射

--- my-config/my-keyboard.el
+++ my-config/my-keyboard.el
@@ -4,18 +4,12 @@
 
 ;;; Code:
 
-;; 使在macOS上，option键是Super，而cmd是Meta
-;; 这样才和Windows键位一致
-;; (setq mac-option-modifier 'super
-;;      mac-command-modifier 'meta)
-
-;; MacBook没有右ctrl键，只有右option键
-;; Emacs大量的命令都是要用ctrl键的，将右option改成右ctrl
-(setq mac-right-option-modifier 'control)
-;; 我其实更想把右shift改为右ctrl的，但似乎没有那么容易
-
-;; macOS中，option键默认是A，即Alt
-;; 需要映射成Super吗
+;; for macOS
+(when (eq system-type 'darwin)
+  ;; macOS上左下角按键依次为fn, super, meta, control
+  ;; 这并不会影响command+tab切换应用
+  (setq mac-control-modifier 'super
+	mac-command-modifier 'control))
 
 (provide 'my-keyboard)
 ;;; my-keyboard.el ends here
