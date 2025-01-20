9d43ff316da80a2365514a89771191887285202e
njw <a@b.com>
2025-01-20 18:00:24 +0800

    update input-method

--- my-proj.el
+++
@@ -1,7 +0,0 @@
-(use-package projectile
-  :init
-  (setq projectile-project-search-path '("~/Developer/" "~/.emacs.d/"))
-  :config
-  (projectil-mode t))
-
-(provide 'my-proj)
--- my/my-input-method.el
+++ my/my-input-method.el
@@ -1,15 +1,17 @@
 ;; 内置输入法
 (use-package pyim
-  :ensure nil
+  :ensure t
   :config
   (setq default-input-method "pyim")
   (pyim-isearch-mode 1)			; 在isearch命令中可以用拼音首字母搜索
   ;; 输入法能够跟随弹窗
   (use-package popup
+    :ensure t
     :config
     (setq pyim-page-tooltip 'popup))
   ;; 五笔词库
   (use-package pyim-wbdict
+    :ensure t
     :config
     (setq pyim-default-scheme 'wubi)
     (pyim-wbdict-v86-enable)))
