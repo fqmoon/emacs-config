09dd6fdca8e83b8010562214cb9906659d274a5a
njw <a@b.com>
2025-01-20 22:21:25 +0800

    org agenda 相关设置

--- my/my-org.el
+++ my/my-org.el
@@ -21,6 +21,24 @@
 	 ("C-c a" . #'org-agenda)
 	 ("C-c c" . #'org-capture)
 	 :map org-mode-map
-	 ("C-j" . avy-goto-char-timer)))
+	 ("C-j" . avy-goto-char-timer))
+  :config
+  ;; org agenda 相关设置
+  (setq org-directory "~/org/")
+  ;; 注意这里并没有设置递归寻找，应该用capture功能将todo项引入
+  (setq org-agenda-files (list (expand-file-name "agenda/" org-directory)
+			       (expand-file-name "todo/" org-directory)
+			       (expand-file-name "notes/" org-directory)
+			       (expand-file-name "journal/" org-directory)))
+  (setq org-capture-templates
+	`(("t" "Todo" entry
+	   (file+headline ,(expand-file-name "todo/todo.org" org-directory) "Tasks")
+	   "* TODO %?\n  %U\n  %a")
+	  ("n" "Notes" entry
+	   (file+datetree ,(expand-file-name "notes/notes.org" org-directory))
+	   "* %?\n  %U\n  %a")
+	  ("j" "Journal" entry
+	   (file+datetree ,(expand-file-name "journal/journal.org" org-directory))
+	   "* %U %?\n  %i\n  %a"))))
 
 (provide 'my-org)
