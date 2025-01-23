20bee79d71467a76a320ffcdf5fe0d32cbbb6d84
njw <a@b.com>
2025-01-23 16:05:09 +0800

    非mac键盘改键

--- my/my-keyboard.el
+++ my/my-keyboard.el
@@ -4,7 +4,22 @@
 
 ;;; Code:
 
-;; for macOS
+;; 非mac键盘
+;; 非mac键盘本身的键位就不太合理，最重要的Ctrl在最左下角，
+;;   而没有用的Win键和Alt键更好按
+;; 应该考虑修改OS本身的键位，而不仅仅是对Emacs修改
+;; 修改的方案中：
+;; - 必改项：左Alt映射成左Ctrl
+;; - 可选项：因为左Alt没有了，可以考虑把左Win或者左Ctrl改成左Alt
+;; 如果使用上面说的这些在OS层面的改键，就不需要在Emacs中改键了
+;; 
+;; 当然有的电脑可能不方便在OS层面改键，那在Emacs之中改也行
+(when (and (not (eq system-type 'darwin))
+	   (file-exists-p (expand-file-name ".keybinding" user-emacs-directory)))
+  (define-key key-translation-map (kbd "<left-alt>") 'control)
+  (define-key key-translation-map (kbd "<left-super>") 'meta))
+
+;; mac键盘
 (when (eq system-type 'darwin)
   ;; 首先在系统层面中将fn与control键交换
   ;; 于是按照下面的映射之后
