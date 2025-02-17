0e81ab53f4deddd391eb3f1ab5ae8d519426badb
njw <njw6622@outlook.com>
2025-02-17 17:47:08 +0800

    添加一个打开资源管理器的函数

--- my/my-base.el
+++ my/my-base.el
@@ -2,10 +2,33 @@
 ;;; 尽量少依赖于三方包
 
 ;; open config file
-(defun open-my-config ()
+(defun my/open-config ()
   "打开配置文件."
   (interactive)
   (find-file user-init-file))
+
+(defun my/path-convert (path)
+  "转换路径斜线为windows风格."
+  (replace-regexp-in-string "/" "\\\\" path))
+
+(defun my/open-explorer ()
+  "打开当前文件所在文件夹."
+  (interactive)
+  (let ((dir (file-name-directory (or buffer-file-name
+				      default-directory)))
+	(dir-cmd (cdr (assoc system-type
+			     '((windows-nt . "explorer")
+			       (darwin . "open")
+			       (gnu/linux . "xdg-open"))))))
+    (cond ((eq dir-cmd nil)
+	   "Unknown system")
+	  (t
+	   (let ((cmd (concat dir-cmd
+			      " \""
+			      (my/path-convert	dir)
+			      "\"")))
+	     (print cmd)
+	     (async-shell-command cmd))))))
 
 ;; magit是一个很好用的版本管理包
 ;; 但是在windows上卡得没法用，在该平台关闭该功能
