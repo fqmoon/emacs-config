939079f4c075429055e544999fd9bdff7106e44a
fqmoon <fqmoon@outlook.com>
2026-01-08 19:00:17 +0800

    apply doom theme

--- my/my-ui.el
+++ my/my-ui.el
@@ -58,7 +58,10 @@
   ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
   ;; (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (doom-themes-org-config)
+  (load-theme 'doom-dark+))
+
+
 
 ;; 标签页
 ;; (use-package tab-bar
