8bb4fe7b0e8a2cb6261ecb47fb96a15dbd8196c3
fqmoon <fqmoon@outlook.com>
2026-02-09 00:01:02 +0800

    export function to disable xterm for TUI

--- my/my-tui.el
+++ my/my-tui.el
@@ -12,14 +12,15 @@
   (my-tui-load-theme))
 
 ;;; 发送禁用焦点事件
-(defun my-tui--disable-xterm ()
+(defun my-tui-disable-xterm ()
   "Disable xterm for TUI."
+  (interactive)
   (send-string-to-terminal "\e[?1004l\e[?1002l\e[?1003l\e[?1005l\e[?1006l"))
 (defun my-tui--tty-setup-func ()
   "Setup tty."
   (when (and (running-in-wsl-p)
 	     (not (display-graphic-p)))
-    (my-tui--disable-xterm)
+    (my-tui-disable-xterm)
     (message "已在WSL2 TUI中禁用焦点事件")))
 (add-hook 'tty-setup-hook #'my-tui--tty-setup-func)
 
