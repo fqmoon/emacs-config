d66ea6dba2bfaf49b0836d6dec4ab5c6930f3174
njw <fqmoon@outlook.com>
2026-03-29 02:06:44 +0800

    shell-mode for tty on w32

--- my/my-term.el
+++ my/my-term.el
@@ -30,10 +30,11 @@
 
 ;;; Code:
 
-(defun my-bash-shell ()
-  "Launch a shell on bash like terminal, 请先 customize 'explicit-shell-file-name'."
-  (interactive)
-  (let* (
+(defun shell-like-tty (shell-program)
+  "Launch a SHELL-PROGRAM on tty like terminal.
+w32不支持tty，无法使用term相关命令，该函数用shell来兼容tty的终端。"
+  (let* ((buf-name (format "*%s*" shell-program))
+	 (buf (get-buffer-create buf-name))
          ;; 1. 强制注入环境变量，覆盖 Bash 的默认行为
          ;; TERM=dumb 告诉 Bash 不要发送复杂的终端控制符
          ;; INSIDE_EMACS 确保子进程知道自己在管道中
@@ -42,12 +43,12 @@
                                  "INSIDE_EMACS=t" 
                                  "PS1=$ ") 
                                process-environment)))
-    
-    ;; 2. 启动 shell
-    (shell "*bash-shell*")
-    
-    ;; 3. 在当前 buffer 开启实时过滤
-    (with-current-buffer "*bash-shell*"
+    (with-current-buffer buf
+      (setq-local explicit-shell-file-name shell-program)
+      ;; 2. 启动 shell
+      (shell buf-name)
+      
+      ;; 3. 在当前 buffer 开启实时过滤
       ;; 解决 ^]0; 这种更新标题的乱码 (ANSI OSC 序列)
       (add-hook 'comint-preoutput-filter-functions
                 (lambda (string)
@@ -58,6 +59,16 @@
       ;; 解决路径和编码
       (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)
       (shell-dirtrack-mode t))))
+
+(defun my-shell-sh ()
+  "Launch a shell on sh."
+  (interactive)
+  (shell-like-tty "sh"))
+
+(defun my-shell-bash ()
+  "Launch a shell on bash."
+  (interactive)
+  (shell-like-tty "bash"))
 
 (provide 'my-term)
 
