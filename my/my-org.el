5e46478e18fd26ff41d5d1a3e1187b123c2aa118
fqmoon <fqmoon@outlook.com>
2026-04-02 23:07:39 +0800

    update org-roam config

--- my/my-org.el
+++ my/my-org.el
@@ -44,21 +44,18 @@
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory "~/org-roam/") ;; 默认笔记目录, 提前手动创建好
-  (org-roam-dailies-directory "daily/") ;; 默认日记目录, 上一目录的相对路径
-  (org-roam-db-gc-threshold most-positive-fixnum) ;; 提高性能
+  (org-roam-directory (file-truename "~/org-roam/"))
+  (org-roam-dailies-directory "daily/")
   :bind (("C-c n f" . org-roam-node-find)
-         ;; 如果你的中文输入法会拦截非 ctrl 开头的快捷键, 也可考虑类似如下的设置
-         ;; ("C-c C-n C-f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
          ("C-c n c" . org-roam-capture)
          ("C-c n l" . org-roam-buffer-toggle) ;; 显示后链窗口
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n j" . org-roam-dailies-goto-date)
          ("C-c n u" . org-roam-ui-mode)) ;; 浏览器中可视化
-  :bind-keymap
-  ("C-c n d" . org-roam-dailies-map) ;; 日记菜单
   :config
-  (require 'org-roam-dailies)  ;; 启用日记功能
-  (org-roam-db-autosync-mode)  ;; 自动同步数据库
-  )
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode))
 
 (provide 'my-org)
