932d5bafa8bc249ca254b93aec6ca11b3e562d9a
fqmoon <fqmoon@outlook.com>
2026-05-06 21:40:11 +0800

    add keybding for gptel

--- my/my-ai.el
+++ my/my-ai.el
@@ -1,5 +1,10 @@
 (use-package gptel
   :ensure t
+  :bind (:map global-map
+	      ("C-c g g" . #'gptel)
+	      ("C-c g s" . #'gptel-send)
+	      ("C-c g a" . #'gptel-add)
+	      ("C-c g m" . #'gptel-menu))
   :config
   (setq
    gptel-model 'gemini-2.5-flash
@@ -8,6 +13,5 @@
 			  (auth-source-pick-first-password
 			   :host "api.generativelanguage.google.com"))
 		   :stream t)))
-
 
 (provide 'my-ai)
